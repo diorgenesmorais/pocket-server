c3d48c0ba0d8d3ec6114465b04c1cc63f6857d9e
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-09-12 04:46:37 -0300

    create goals route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,26 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import z from 'zod'
+import { createGoal } from '../functions/create-goal'
 
-const app = fastify();
+const app = fastify()
 
-app
-	.listen({
-		port: 3000,
-	})
-	.then(() => {
-		console.log("HTTP server running!");
-	});
+app.post('/goals', async (request, response) => {
+    const createGoalSchema = z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+    })
+
+    const { title, desiredWeeklyFrequency } = createGoalSchema.parse(
+        request.body
+    )
+
+    const result = await createGoal({ title, desiredWeeklyFrequency })
+
+    response.status(201).send({ ...result })
+})
+
+app.listen({
+    port: 3000,
+}).then(() => {
+    console.log('HTTP server running!')
+})

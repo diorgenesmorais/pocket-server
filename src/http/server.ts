9f86fff454e2f509bcb03f9f9aaed7d56f2d53c6
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-09-12 18:25:41 -0300

    refactor: create goal route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,5 @@
 import fastify from 'fastify'
 import z from 'zod'
-import { createGoal } from '../functions/create-goal'
 import {
     serializerCompiler,
     validatorCompiler,
@@ -8,20 +7,14 @@ import {
 } from 'fastify-type-provider-zod'
 import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 import { createGoalCompletion } from '../functions/create-goal completion'
+import { createGoalRoute } from './routes/create-goal.route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-const fastifyValidatorWithZod = {
-    schema: {
-        body: z.object({
-            title: z.string(),
-            desiredWeeklyFrequency: z.number().int().min(1).max(7),
-        }),
-    },
-}
+app.register(createGoalRoute)
 
 const completionValidator = {
     schema: {
@@ -30,14 +23,6 @@ const completionValidator = {
         }),
     },
 }
-
-app.post('/goals', fastifyValidatorWithZod, async (request, response) => {
-    const { title, desiredWeeklyFrequency } = request.body
-
-    const result = await createGoal({ title, desiredWeeklyFrequency })
-
-    response.status(201).send({ ...result })
-})
 
 app.get('/pending-goals', async () => {
     const { pendingGoals } = await getWeekPendingGoals()

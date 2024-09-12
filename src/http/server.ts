6124b03b2516fa37ae1be93d71166abf5fe6042c
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-09-12 20:38:21 -0300

    add fastify cors lib

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,8 +8,13 @@ import { createGoalRoute } from './routes/create-goal.route'
 import { getPendingGoalsRoute } from './routes/get-pending-goals.route'
 import { completionsRoute } from './routes/completions.route'
 import { getWeekSummaryRoute } from './routes/get-week-summary.route'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
